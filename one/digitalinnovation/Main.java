06386be6198495b337e29f2eab1538126be989f8
filipeoliveira1000 <filipeoliveira1000@yahoo.com.br>
2022-05-11 14:46:33 -0300

    First commit

---
+++ one/digitalinnovation/Main.java
@@ -0,0 +1,23 @@
+package one.digitalinnovation;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Exercício calculadora");
+        Calculadora.soma(3, 6);
+        Calculadora.subtracao(9, 1.8);
+        Calculadora.multiplicacao(7, 8);
+        Calculadora.divisao(5, 2.5);
+
+        System.out.println("Exercício mensagem");
+        Mensagem.obterMensagem(9);
+        Mensagem.obterMensagem(14);
+        Mensagem.obterMensagem(1);
+
+        System.out.println("Exercício empréstimo");
+        Emprestimo.calcular(1000, Emprestimo.getDuasParcelas());
+        Emprestimo.calcular(1000, Emprestimo.getTresParcelas());
+        Emprestimo.calcular(1000, 5);
+    }
+}
